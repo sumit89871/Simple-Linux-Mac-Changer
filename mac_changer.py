71cdc62dc28683db16c1b27cdfa47640eb71a67a
Sumit Kumar <77721240+sumit89871@users.noreply.github.com>
2022-02-22 17:33:56 +0530

    Update mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,5 @@
 
 import subprocess
 
-subprocess.call("ifconfig", shell=True)
 subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:99", shell=True)
-subprocess.call("ifconfig eth0", shell=True)
+subprocess.call("ifconfig", shell=True)
